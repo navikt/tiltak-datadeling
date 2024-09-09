677453a9c05b1595fe1f722a3a5198776616ac82
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-09-09 14:27:49 +0200

    Bytt gruppe og reset til tidligste

--- src/main/kotlin/no/nav/tiltak/datadeling/kafka/TiltakHendelseLytter.kt
+++ src/main/kotlin/no/nav/tiltak/datadeling/kafka/TiltakHendelseLytter.kt
@@ -1,14 +1,12 @@
 package no.nav.tiltak.datadeling.kafka
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import jakarta.annotation.PostConstruct
 import no.nav.tiltak.datadeling.AvtaleRepository
 import no.nav.tiltak.datadeling.domene.Avtale
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.AbstractConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 import java.time.Duration
@@ -19,7 +17,7 @@ import java.time.Duration
 class TiltakHendelseKafkaKonsument(
     val mapper: ObjectMapper,
     val avtaleRepository: AvtaleRepository
-) : AbstractConsumerSeekAware() {
+) {
     val log = LoggerFactory.getLogger(javaClass)
 
     @KafkaListener(topics = ["\${tiltak-datadeling.kafka.topic}"])
@@ -33,12 +31,5 @@ class TiltakHendelseKafkaKonsument(
             log.error("Feil oppstod ved henting av kafkamelding", ex)
             acknowledgment.nack(Duration.ofSeconds(5))
         }
-    }
-
-    @PostConstruct
-    fun init() {
-        log.info("SEEKER TIL START")
-        log.info("Assignments: ${callbacksAndTopics.map { it.value }}")
-        seekToBeginning()
     }
 }
