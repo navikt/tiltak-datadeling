97c6c7e7edc1c39787f11896c5e078af35e2a911
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-11-22 10:19:36 +0100

    Legg til VTAO-tiltakstype

--- src/main/kotlin/no/nav/tiltak/datadeling/domene/Tiltakstype.kt
+++ src/main/kotlin/no/nav/tiltak/datadeling/domene/Tiltakstype.kt
@@ -1,14 +1,11 @@
 package no.nav.tiltak.datadeling.domene
 
-enum class Tiltakstype(
-    beskrivelse: String,
-    behandlingstema: String,
-    tiltakskodeArena: String?
-) {
-    ARBEIDSTRENING("Arbeidstrening", "ab0422", "ARBTREN"),
-    MIDLERTIDIG_LONNSTILSKUDD("Midlertidig lønnstilskudd", "ab0336", "MIDLONTIL"),
-    VARIG_LONNSTILSKUDD("Varig lønnstilskudd", "ab0337", "VARLONTIL"),
-    MENTOR("Mentor", "ab0416", "MENTOR"),
-    INKLUDERINGSTILSKUDD("Inkluderingstilskudd", "ab0417", "INKLUTILS"),
-    SOMMERJOBB("Sommerjobb", "ab0450", null);
+enum class Tiltakstype {
+    ARBEIDSTRENING,
+    MIDLERTIDIG_LONNSTILSKUDD,
+    VARIG_LONNSTILSKUDD,
+    MENTOR,
+    INKLUDERINGSTILSKUDD,
+    SOMMERJOBB,
+    VTAO;
 }
