0b69502185eaa99a36476d8cd2385b85562ba21e
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-01-23 09:19:57 +0100

    Ikke rull tilbake posisjon i kafka

--- src/main/kotlin/no/nav/tiltak/datadeling/kafka/TiltakHendelseLytter.kt
+++ src/main/kotlin/no/nav/tiltak/datadeling/kafka/TiltakHendelseLytter.kt
@@ -5,14 +5,12 @@ import no.nav.tiltak.datadeling.AvtaleRepository
 import no.nav.tiltak.datadeling.FeiledeMeldingerRepository
 import no.nav.tiltak.datadeling.domene.Avtale
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.TopicPartition
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.listener.AbstractConsumerSeekAware
-import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
@@ -33,16 +31,11 @@ class TiltakHendelseKafkaKonsument(
             log.info("Mottatt melding på topic")
             val avtale = mapper.readValue(record.value(), Avtale::class.java)
             avtaleRepository.save(avtale)
-            acknowledgment.acknowledge()
         } catch (ex: Exception) {
             log.error("Feil oppstod ved henting av kafkamelding", ex)
             feiledeMeldingerRepository.save(record, ex)
+        } finally {
             acknowledgment.acknowledge()
         }
-    }
-
-    override fun onPartitionsAssigned(assignments: MutableMap<TopicPartition, Long>, callback: ConsumerSeekAware.ConsumerSeekCallback) {
-        super.onPartitionsAssigned(assignments, callback)
-        callback.seekToBeginning(assignments.keys)
     }
 }
