2c474ec7459cb43f9c2cf0ff1ee6a93c1dfe7b28
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-01-29 22:07:20 +0100

    Kafka-replay skal ikke revertere avtaler
    Hvis en avtale har et gitt versjonsnummer (eller er
    sist endret på en gitt dato), ikke oppdater avtalen
    med data fra en tidligere versjon av avtalen.

--- src/main/kotlin/no/nav/tiltak/datadeling/kafka/TiltakHendelseKafkaKonsument.kt
+++ src/main/kotlin/no/nav/tiltak/datadeling/kafka/TiltakHendelseKafkaKonsument.kt
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.tiltak.datadeling.AvtaleRepository
 import no.nav.tiltak.datadeling.FeiledeMeldingerRepository
 import no.nav.tiltak.datadeling.domene.Avtale
+import no.nav.tiltak.datadeling.toOsloOffset
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.listener.AbstractConsumerSeekAware
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
@@ -30,7 +33,13 @@ class TiltakHendelseKafkaKonsument(
         try {
             log.info("Mottatt melding på topic")
             val avtale = mapper.readValue(record.value(), Avtale::class.java)
-            avtaleRepository.save(avtale)
+            log.info("Lagrer avtale {}, endret {}", avtale.avtaleId, avtale.sistEndret)
+            val lagretAvtale = avtaleRepository.save(avtale)
+            if (lagretAvtale == null) {
+                log.info("Avtale {} ble ikke oppdatert", avtale.avtaleId)
+            } else if (lagretAvtale.endretTidspunkt == avtale.sistEndret.toOsloOffset()) {
+                log.info("Avtale {} ble oppdatert, samme endret-tidspunkt", avtale.avtaleId)
+            }
         } catch (ex: Exception) {
             log.error("Feil oppstod ved henting av kafkamelding", ex)
             feiledeMeldingerRepository.save(record, ex)
@@ -40,8 +49,8 @@ class TiltakHendelseKafkaKonsument(
         }
     }
 
-//    override fun onPartitionsAssigned(assignments: MutableMap<TopicPartition, Long>, callback: ConsumerSeekAware.ConsumerSeekCallback) {
-//        super.onPartitionsAssigned(assignments, callback)
-//        callback.seekToBeginning(assignments.keys)
-//    }
+    override fun onPartitionsAssigned(assignments: MutableMap<TopicPartition, Long>, callback: ConsumerSeekAware.ConsumerSeekCallback) {
+        super.onPartitionsAssigned(assignments, callback)
+        callback.seekToBeginning(assignments.keys)
+    }
 }
