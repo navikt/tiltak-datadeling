99aa37d0bcb71c7245c3260bb0dc84a1b631ccc5
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-06-04 13:00:04 +0200

    Lagt til datastream-migrering

---
+++ src/main/resources/db/migration/V03__datastream.sql
@@ -0,0 +1,24 @@
+-- [jooq ignore start]
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            ALTER USER "tiltak-datadeling" WITH REPLICATION;
+            CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
+
+            ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
+            GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
+            ALTER USER "datastream" WITH REPLICATION;
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+-- [jooq ignore stop]
